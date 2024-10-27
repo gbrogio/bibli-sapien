75c098180c4ac9cb3af97b6eea70cb520a132a40
Guilherme Brogio <guilhermebrogio.ps@gmail.com>
2024-10-27 09:20:49 -0300

    removendo imports não utilizados

--- src/main/java/com/application/App.java
+++ src/main/java/com/application/App.java
@@ -2,10 +2,8 @@ package com.application;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
-import javafx.geometry.Rectangle2D;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.stage.Screen;
 import javafx.stage.Stage;
 import javafx.stage.StageStyle;
 
