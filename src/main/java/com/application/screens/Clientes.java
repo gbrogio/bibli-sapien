6fa82c5fe33db2831be78dbc11a0d16b6ba7d2f6
Guilherme Brogio <guilhermebrogio.ps@gmail.com>
2024-10-27 09:08:55 -0300

    adicionado o controlado geral as outras telas

--- src/main/java/com/application/screens/Clientes.java
+++ src/main/java/com/application/screens/Clientes.java
@@ -1,10 +1,11 @@
 package com.application.screens;
 
+import com.application.ControladorTela;
 import com.application.Database.Database;
 import java.io.IOException;
 import javafx.fxml.FXML;
 
-public class Clientes {
+public class Clientes extends ControladorTela {
     Database db = Database.getInstance();
 
     @FXML
--- src/main/java/com/application/screens/Livros.java
+++ src/main/java/com/application/screens/Livros.java
@@ -1,10 +1,12 @@
 package com.application.screens;
 
 import java.io.IOException;
+
+import com.application.ControladorTela;
 import com.application.Database.Database;
 import javafx.fxml.FXML;
 
-public class Livros {
+public class Livros extends ControladorTela {
     Database db = Database.getInstance();
 
     @FXML
